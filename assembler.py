135f1a4262840b182ef411453c3e7f8bba6aa321
Edward Stables <edward.stables1198@gmail.com>
2024-07-06 00:17:25 +0100

    don't give instructions their whole own class, just a function to do the same job. Generate basic encoding, not including lables

--- assembler.py
+++ assembler.py
@@ -83,29 +83,13 @@ class SourceLine:
 
         return ret
             
-
-class ProgramInstruction:
-    def __init__(self, sourceline: SourceLine, isa: ISADefinition):
-        self.sourceline = sourceline
-        self.isa = isa
-        #assume all inputs are well-formed instructions, not empty or labels
-        self.ops = [i for i in self.sourceline.source.split() if i]
-        self._encode()
-
-    def _encode(self) -> bool:
-        valid, result = self.isa.match(self.ops[0], *self.ops[1:])
-        if not valid:
-            operand_index, msg = result
-            msg = self.sourceline.annotate(operand_index=operand_index, msg=msg)
-            print(msg)
-        
-
 class Block:
     pass
 
 class Program:
     def __init__(self, file: Path, isa: ISADefinition):
         self.file = file
+        self.isa = isa
         self.source_lines: list[SourceLine] = []
         with self.file.open() as f:
             for i, line in enumerate(f):
@@ -116,10 +100,53 @@ class Program:
                     print(msg)
                 self.source_lines.append(sl)
         
+        self.instructions = []
+        error_count = 0
         for sl in self.source_lines:
             if not sl.is_instr(): continue
-            ProgramInstruction(sl, isa)
+            
+            #Valid is bool.
+            #Encoding is 1-4 entry list. Opcode, registers, and immediates are encoded.
+            #Label ref gives an index of the encoding that contains a label. 0 for none
+            valid, encoding, label_ref = self.get_encoding(sl)
+            
+            if valid:
+                self.instructions.append((sl,encoding,label_ref))
+            else:
+                error_count+=1
 
+            if error_count > 5:
+                print("Reached max error count, exiting.")
+                exit(1)
+
+        for sl, encoding, label_ref in self.instructions:
+            print(encoding, label_ref)
+
+
+        
+    def get_encoding(self, sourceline):
+        #assume all inputs are well-formed instructions, not empty or labels
+        ops = [i for i in sourceline.source.split() if i]
+        valid, result = self.isa.match(ops[0], *ops[1:])
+        if not valid:
+            operand_index, msg = result
+            msg = sourceline.annotate(operand_index=operand_index, msg=msg)
+            print(msg)
+            return False, None, None
+
+        instr_def = result
+        label_index = 0
+        encoding = [instr_def.encoding] 
+        for i, o in enumerate(instr_def.ops):
+            match o:
+                case instructions.OperandType.REG:
+                    encoding.append(self.isa.get_reg_encoding(ops[i+1]))
+                case instructions.OperandType.IMM:
+                    encoding.append(int(ops[i+1]))
+                case instructions.OperandType.LAB:
+                    encoding.append(ops[i+1])
+                    label_index = i+1
+        return True, encoding, label_index
 
 def get_args():
     parser = ArgumentParser()
