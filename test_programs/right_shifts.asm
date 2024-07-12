8f6af4692f21cf48ba284119b69af7963fc47b1e
Edward Stables <edward.stables1198@gmail.com>
2024-07-12 22:35:37 +0100

    right shift test program

---
+++ test_programs/right_shifts.asm
@@ -0,0 +1,17 @@
+    MOV 8
+    ASR 3
+    MOV R1 ACC
+
+    MOV 8
+    MOV R8 1
+    ASR R8
+    MOV R2 ACC
+
+    MOV 8
+    LSR 3
+    MOV R3 ACC
+
+    MOV 8
+    MOV R8 1
+    LSR R8
+    MOV R4 ACC
