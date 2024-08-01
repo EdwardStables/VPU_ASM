39ddfdd7ba84a862a4a93a47c3082bc23a7f6392
Edward Stables <edward.stables1198@gmail.com>
2024-08-01 23:20:09 +0100

    use hex literals in dma_set

--- test_programs/dma_set.asm
+++ test_programs/dma_set.asm
@@ -2,12 +2,12 @@
     ; Set address 0xFFF00000 to 0xFFF10000 to 0xFF
     ;
 
-    MOV 4095            ; 0xFFF in ACC
+    MOV 0xFFF           ; 0xFFF in ACC
     LSL 20              ; 0xFFF00000 in ACC
     P.DMA.DST ACC
     
-    MOV 65535           ; Length is 0x10000-1
+    MOV 0x10000         ; Length is 0x10000
     P.DMA.LEN ACC
     
-    MOV 15              ; Set all bytes to 0xFF
+    MOV 0xFF            ; Set all bytes to 0xFF
     P.DMA.SET ACC       
