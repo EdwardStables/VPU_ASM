7b11be9b6583bf40acfa09e71ab1d0d7b2afb418
Edward Stables <edward.stables1198@gmail.com>
2025-01-26 23:35:20 +0000

    Update comments and set DST for indexing operations

--- test_programs/matrix_ops.asm
+++ test_programs/matrix_ops.asm
@@ -3,27 +3,27 @@
     P.MAT.DST SP 0         ; Write to SP
     P.MAT.OPR IDENTITY_MAT ; Set to identity
     
-    MOV 2           ; Set 1,2 to 2
+    MOV 0x20        ; Set 1,2 to 2
     P.MAT.ROW 1
     P.MAT.COL 2
     P.MAT.OPR SET_MAT
-    ADD 1           ; Set 1,3 to 3
+    MOV 0x30        ; Set 1,3 to 3
     P.MAT.COL 3
     P.MAT.OPR SET_MAT
-    ADD 1           ; Set 1,4 to 3
+    MOV 0x40        ; Set 1,4 to 4
     P.MAT.COL 4
     P.MAT.OPR SET_MAT
 
-    ADD 1           ; Add 4 to first row and copy to 2nd row
+    MOV 0x40        ; Add 4 to first row and copy to 2nd row
     P.MAT.SRC1 SP 0
     P.MAT.DST SP 8
     P.MAT.OPR ADD_VEC_SCALAR
-    ADD 4           ; Add 4 to first row and copy to 3rd row
-    P.MAT.SRC1 SP 0
+    
+    P.MAT.SRC1 SP 8 ; Add 4 to the 2nd row and copy to the 3rd row
     P.MAT.DST SP 16
     P.MAT.OPR ADD_VEC_SCALAR
-    ADD 4           ; Add 4 to first row and copy to 4th row
-    P.MAT.SRC1 SP 0
+    
+    P.MAT.SRC1 SP 16; Add 4 to first row and copy to 4th row
     P.MAT.DST SP 24
     P.MAT.OPR ADD_VEC_SCALAR
 
